c9e970e311ad2ea4d261c917d0947aa414fdedb0
Naoki FUJIEDA <nf.longarch@gmail.com>
2023-03-27 13:16:44 +0900

    Cleaning up the code for logging

--- scripts/AIS31.rb
+++ scripts/AIS31.rb
@@ -13,136 +13,108 @@ class Ais31
   RUN_MAX = [2733, 1421, 748, 402, 223, 223]
   Q_T8 = 2560
   K_T8 = 256000
+
+  def self.report (text_line, out_file = nil, out_console = STDOUT)
+    @@out_file = out_file if out_file
+    out_console.puts text_line
+    @@out_file.puts text_line
+  end
+
+  def self.report_error (text_line, out_file = nil)
+    report text_line, out_file, STDERR
+  end
   
-  def self.procedure_a (bits, filePath)
+  def self.procedure_a (bits, out_file)
     if bits.length < BIT_PROC_A
-      text_line = "!! The input bitstring is too short. Stop."
-      STDERR.puts text_line
-      File.open(filePath , "a+") { |f| f.puts(text_line) }
+      report_error "!! The input bitstring is too short. Stop.", out_file
       exit 1
     end
-    text_line = "[[ AIS-31 Test Procedure A ]]"
-    puts text_line
-    File.open(filePath , "a+") { |f| f.puts(text_line) }
+    report "[[ AIS-31 Test Procedure A ]]", out_file
     failure = nil
     2.times do |trial|
       results = Array.new
-      results += self.test_t0(bits, filePath)[0]
+      results += self.test_t0(bits, out_file)[0]
       bits = bits[BIT_TEST_T0..-1]
       257.times do |i|
-        text_line = "[Iteration %3d]" % (i + 1)
-        File.open(filePath , "a+") { |f| f.puts(text_line) }
-        puts text_line
-          [:test_t1, :test_t2, :test_t3, :test_t4, :test_t5].each do |t|
-          results += self.method(t).call(bits, filePath)[0]           
+        report "[Iteration %3d]" % (i + 1)
+        [:test_t1, :test_t2, :test_t3, :test_t4, :test_t5].each do |t|
+          results += self.method(t).call(bits, out_file)[0]           
         end
         bits = bits[BIT_TEST_A..-1]
       end
       failure = results.reduce(0){|s, x| s + ((x == "FAIL") ? 1 : 0) }
-      text_line = "## Number of FAILed Tests (out of 1286): %d" % failure
-      puts
-      puts text_line
-      File.open(filePath , "a+") { |f| f.puts("") }
-      File.open(filePath , "a+") { |f| f.puts(text_line) }
+      report ""
+      report "## Number of FAILed Tests (out of 1286): %d" % failure
       break if failure != 1 || trial != 0 || bits.length < BIT_PROC_A
-      text_line = "## Exactly one test has failed. Retrying..."
-      puts text_line
-      File.open(filePath , "a+") { |f| f.puts(text_line) }
+      report "## Exactly one test has failed. Retrying..."
     end
     if failure == 0
-      text_line = "## Test Procedure A is PASSED!!!"
-      puts text_line
-      puts
-      File.open(filePath , "a+") { |f| f.puts(text_line) }
-      File.open(filePath , "a+") { |f| f.puts("") }
+      report "## Test Procedure A is PASSED!!!"
+      report ""
       return [["PASS"], BIT_PROC_A]
     else
-      text_line = "## Test Procedure A is FAILED..."
-      puts text_line
-      puts
-      File.open(filePath , "a+") { |f| f.puts(text_line) }
-      File.open(filePath , "a+") { |f| f.puts("") }
+      report "## Test Procedure A is FAILED..."
+      report ""
       return [["FAIL"], BIT_PROC_A]
     end
   end
 
-  def self.procedure_b (bits, filePath)
-    text_line = "[[ AIS-31 Test Procedure B ]]"
-    puts text_line
-    File.open(filePath , "a+") { |f| f.puts(text_line) }
+  def self.procedure_b (bits, out_file)
+    report "[[ AIS-31 Test Procedure B ]]", out_file
     failure = nil
     total = bits.length
     2.times do |trial|
       results = Array.new
       [:test_t6, :test_t7a, :test_t7b, :test_t7c, :test_t8].each do |t|
-        result = self.method(t).call(bits, filePath)
+        result = self.method(t).call(bits, out_file)
         results += result[0]
         bits = bits[result[1]..-1]
       end
       failure = results.reduce(0){|s, x| s + ((x == "FAIL") ? 1 : 0) }
-      text_line = "## Number of bits used: %d" % (total - bits.length)
-      puts
-      puts text_line
-      File.open(filePath , "a+") { |f| f.puts("") }
-      File.open(filePath , "a+") { |f| f.puts(text_line) }
-      text_line = "## Number of FAILed Tests (out of 9): %d" % failure
-      puts text_line
-      File.open(filePath , "a+") { |f| f.puts(text_line) }
+      report ""
+      report "## Number of bits used: %d" % (total - bits.length)
+      report "## Number of FAILed Tests (out of 9): %d" % failure
       break if failure != 1 || trial != 0
-      text_line = "## Exactly one test has failed. Retrying..."
-      puts text_line
-      File.open(filePath , "a+") { |f| f.puts(text_line) }
+      report "## Exactly one test has failed. Retrying..."
     end
     if failure == 0
-      text_line = "## Test Procedure B is PASSED!!!"
-      puts text_line
-      puts
-      File.open(filePath , "a+") { |f| f.puts(text_line) }
-      File.open(filePath , "a+") { |f| f.puts("") }
+      report "## Test Procedure B is PASSED!!!"
+      report ""
       return [["PASS"], total - bits.length]
     else
-      text_line = "## Test Procedure B is FAILED..."
-      puts text_line
-      puts
-      File.open(filePath , "a+") { |f| f.puts(text_line) }
-      File.open(filePath , "a+") { |f| f.puts("") }
+      report "## Test Procedure B is FAILED..."
+      report ""
       return [["FAIL"], total - bits.length]
     end
   end
 
-  def self.test_t0 (bits, filePath)
+  def self.test_t0 (bits, out_file)
     bits = bits[0..BIT_TEST_T0-1]
     num_unique = bits.scan(/.{48}/).uniq.size
     result = (num_unique == 65536) ? "PASS" : "FAIL"
-    text_line = "T0 : %s => # of unique bitstrings = %d (65536)" % [result, num_unique]
-    puts text_line
-    File.open(filePath , "a+") { |f| f.puts(text_line) }
+    report "T0 : %s => # of unique bitstrings = %d (65536)" % [result, num_unique], out_file
     return [[result], BIT_TEST_T0]
   end
 
-  def self.test_t1 (bits, filePath)
+  def self.test_t1 (bits, out_file)
     bits = bits[0..BIT_TEST_A-1]
     ones = bits.bytes.reduce(0){|s, x| s + (x & 1) }
     result = (ones >= 9655 && ones <= 10345) ? "PASS" : "FAIL"
-    text_line = "T1 : %s => # of ones = %d (9655-10345)" % [result, ones]
-    puts text_line
-    File.open(filePath , "a+") { |f| f.puts(text_line) }
+    report "T1 : %s => # of ones = %d (9655-10345)" % [result, ones], out_file
     return [[result], BIT_TEST_A]
   end
 
-  def self.test_t2 (bits, filePath)
+  def self.test_t2 (bits, out_file)
     bits = bits[0..BIT_TEST_A-1]
     freq = Array.new(16, 0)
     bits.scan(/..../).each{|x| freq[x.to_i(2)] += 1 }
     t2 = 16.0 / 5000 * freq.reduce(0){|s, x| s + x * x } - 5000
     result = (t2 > 1.03 && t2 < 57.4) ? "PASS" : "FAIL"
-    text_line = "T2 : %s => Test value = %.3f (1.03-57.4)" % [result, t2]
-    puts text_line
-    File.open(filePath , "a+") { |f| f.puts(text_line) }
+    report "T2 : %s => Test value = %.3f (1.03-57.4)" % [result, t2], out_file
     return [[result], BIT_TEST_A]
   end
 
-  def self.test_t3 (bits, filePath)
+  def self.test_t3 (bits, out_file)
     bits = bits[0..BIT_TEST_A-1].bytes.map{|x| x & 1 }
     runs = Array.new
     runs[0] = Array.new 6, 0
@@ -164,25 +136,17 @@ class Ais31
       run.each_index {|i| result = "FAIL" if run[i] < RUN_MIN[i] || run[i] > RUN_MAX[i] }
     end
     text_line = "T3 : %s => Run |" % result
-    File.open(filePath , "a+") { |f| f.print(text_line) }
-    print "T3 : %s => Run |" % result
-    runs[0].each_index{|i| print " %d(%4d-%4d) |" % [i + 1, RUN_MIN[i], RUN_MAX[i]] ; text_line = " %d(%4d-%4d) |" % [i + 1, RUN_MIN[i], RUN_MAX[i]] ; File.open(filePath , "a+") { |f| f.print(text_line) }}
-    puts
+    runs[0].each_index{|i| text_line += " %d(%4d-%4d) |" % [i + 1, RUN_MIN[i], RUN_MAX[i]] }
+    report text_line, out_file
     runs.each_index do |i|
-      File.open(filePath , "a+") { |f| f.puts("") }   
       text_line = "             %ds  |" % i
-      File.open(filePath , "a+") { |f| f.print(text_line) }
-      # print "             %ds  |" % i
-      # out_file = File.new(filePath, "a++")
-      # runs[i].each{|x| print " %11d  |" % x}
-      runs[i].each{|x| print " %11d  |" % x; text_line = " %11d  |" % x; File.open(filePath , "a+") { |f| f.print(text_line) }}
-      # runs[i].each{|x| print " %11d  |" % x; text_line = " %11d  |" % x; out_file.puts(text_line)}
-      puts
+      runs[i].each{|x| text_line += " %11d  |" % x}
+      report text_line
     end
     return [[result], BIT_TEST_A]
   end
 
-  def self.test_t4 (bits, filePath)
+  def self.test_t4 (bits, out_file)
     bits = bits[0..BIT_TEST_A-1].bytes.map{|x| x & 1 }
     cur = bits[0]
     len = 1
@@ -197,14 +161,11 @@ class Ais31
       end
     end
     result = (longest <= 33) ? "PASS" : "FAIL"
-    text_line = "T4 : %s => Longest run = %d (1-33)" % [result, longest]    
-    puts text_line 
-    File.open(filePath , "a+") { |f| f.puts("") }   
-    File.open(filePath , "a+") { |f| f.puts(text_line) }   
+    report "T4 : %s => Longest run = %d (1-33)" % [result, longest], out_file
     return [[result], BIT_TEST_A]
   end
 
-  def self.test_t5 (bits, filePath)
+  def self.test_t5 (bits, out_file)
     bits = bits[0..BIT_TEST_A-1].bytes.map{|x| x & 1 }
     tau = 0
     max_dif = -1
@@ -218,23 +179,19 @@ class Ais31
     end
     t5 = (10000..14999).to_a.reduce(0){|s, x| s + (bits[x] ^ bits[x + tau]) }
     result = (t5 >= 2327 && t5 <= 2673) ? "PASS" : "FAIL"
-    text_line = "T5 : %s => value of T5(%d) = %d (2327-2673)" % [result, tau, t5]
-    puts text_line
-    File.open(filePath , "a+") { |f| f.puts(text_line) }
+    report "T5 : %s => value of T5(%d) = %d (2327-2673)" % [result, tau, t5], out_file
     return [[result], BIT_TEST_A]
   end
   
-  def self.test_t6 (bits, filePath)
+  def self.test_t6 (bits, out_file)
     bits = bits[0..BIT_TEST_B-1]
     ones = bits.bytes.reduce(0){|s, x| s + (x & 1) }
     result = (ones >= 47500 && ones <= 52500) ? "PASS" : "FAIL"
-    text_line = "T6 : %s => # of ones = %d (47500-52500)" % [result, ones]
-    puts text_line
-    File.open(filePath , "a+") { |f| f.puts(text_line) }
+    report "T6 : %s => # of ones = %d (47500-52500)" % [result, ones], out_file
     return [[result], BIT_TEST_B]
   end
 
-  def self.collect_tuples (bits, len, filePath)
+  def self.collect_tuples (bits, len, out_file)
     num_tf = 2 ** (len - 1)
     total  = 0
     rest   = BIT_TEST_B * num_tf;
@@ -242,9 +199,7 @@ class Ais31
     ones   = Array.new(num_tf, 0)
     while rest > 0
       if total + len > bits.length
-        text_line = "!! The input bitstring ran out. Stop."
-        STDERR.puts text_line
-        File.open(filePath , "a+") { |f| f.puts(text_line) }
+        report_error "!! The input bitstring ran out. Stop.", out_file
         exit 1
       end
       tuple = bits[total..total+len-1]
@@ -259,19 +214,18 @@ class Ais31
     return [ones, total]
   end
 
-  def self.test_t7a (bits, filePath)
-    ones, total = collect_tuples(bits, 2, filePath)
+  def self.test_t7a (bits, out_file)
+    ones, total = collect_tuples(bits, 2, out_file)
     vemp01 = ones[0].fdiv(BIT_TEST_B)
     vemp10 = 1.0 - ones[1].fdiv(BIT_TEST_B)
     result = ((vemp01 + vemp10 - 1).abs < 0.02) ? "PASS" : "FAIL"
-    text_line = "T7a: %s => vemp0(1) = %.5f, vemp1(0) = %.5f, sum = %.5f (0.98-1.02)" % [result, vemp01, vemp10, vemp01 + vemp10]
-    puts text_line
-    File.open(filePath , "a+") { |f| f.puts(text_line) }
+    report "T7a: %s => vemp0(1) = %.5f, vemp1(0) = %.5f, sum = %.5f (0.98-1.02)" %
+      [result, vemp01, vemp10, vemp01 + vemp10], out_file
     return [[result], total]
   end
 
-  def self.test_t7 (bits, filePath, postfix, len)
-    ones, total = collect_tuples(bits, len, filePath)
+  def self.test_t7 (bits, out_file, postfix, len)
+    ones, total = collect_tuples(bits, len, out_file)
     num_cp = ones.size / 2
     results = Array.new
     num_cp.times do |i|      
@@ -284,23 +238,21 @@ class Ais31
       chi2 = ((f00 - np0) ** 2 + (f10 - np0) ** 2) / np0 +
              ((f01 - np1) ** 2 + (f11 - np1) ** 2) / np1
       result = (chi2 <= 15.136) ? "PASS" : "FAIL"
-      text_line = "T7%s: %s => vemp%%0%db(1) = %.5f, vemp%%0%db(1) = %.5f, chi-sq = %.3f (0-15.136)" % [postfix, result, len - 1, f01.fdiv(BIT_TEST_B), len - 1, f11.fdiv(BIT_TEST_B), chi2] % [i, i + num_cp]
-      puts text_line
-      File.open(filePath , "a+") { |f| f.puts(text_line) }
+      report "T7%s: %s => vemp%%0%db(1) = %.5f, vemp%%0%db(1) = %.5f, chi-sq = %.3f (0-15.136)" %
+        [postfix, result, len - 1, f01.fdiv(BIT_TEST_B), len - 1, f11.fdiv(BIT_TEST_B), chi2] %
+        [i, i + num_cp], out_file
       results << result
     end
     return [results, total]
   end
   
-  def self.test_t7b (bits, filePath) self.test_t7(bits, filePath, 'b', 3) end
-  def self.test_t7c (bits, filePath) self.test_t7(bits, filePath, 'c', 4) end
+  def self.test_t7b (bits, out_file) self.test_t7(bits, out_file, 'b', 3) end
+  def self.test_t7c (bits, out_file) self.test_t7(bits, out_file, 'c', 4) end
 
-  def self.test_t8 (bits, filePath, k = K_T8)
+  def self.test_t8 (bits, out_file, k = K_T8)
     bit_test_q8 = (Q_T8 + k) * 8
     if bits.length < bit_test_q8
-      text_line = "!! The input bitstring is too short. Stop."
-      STDERR.puts text_line
-      File.open(filePath , "a+") { |f| f.puts(text_line) }
+      report_error "!! The input bitstring is too short. Stop.", out_file
       exit 1
     end
     bits = bits[0..bit_test_q8-1].scan(/.{8}/).map{|x| x.to_i(2) }
@@ -322,9 +274,7 @@ class Ais31
     end
     entropy = g_sum / k / Math.log(2) / 8
     result = (entropy > 0.997) ? "PASS" : "FAIL"
-    text_line = "T8 : %s => estimated entropy per bit = %.6f (0.997-)" % [result, entropy]
-    puts text_line
-    File.open(filePath , "a+") { |f| f.puts(text_line) }
+    report "T8 : %s => estimated entropy per bit = %.6f (0.997-)" % [result, entropy], out_file
     return [[result], bit_test_q8]
   end
 end
--- scripts/ais_test.rb
+++ scripts/ais_test.rb
@@ -1,13 +1,16 @@
 # AIS-31 test suite frontend
-# 2020-02-19 Naoki F., AIT
+# 2020-02-19 - 2023-03-27 Naoki F., AIT
 # New BSD License is applied. See COPYING file for details.
 
 require_relative "AIS31.rb"
 
 TESTS = ['ALL', 'A', 'B', 'T0', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T8L']
 t = ARGV[0].upcase
-if (ARGV.size != 4 || ! TESTS.include?(t) || ARGV[2].to_s != '-o') && (ARGV.size != 2 || ! TESTS.include?(t))
-  STDERR.puts "usage: ruby ais_test.rb TEST FILE"
+outpath = (ARGV.size == 4 && ARGV[2] == '-o') ? ARGV[3] :
+          (ARGV.size == 2)                    ? "output.txt" : nil
+
+if (! outpath || ! TESTS.include?(t))
+  STDERR.puts "usage: ruby ais_test.rb TEST FILE [-o LOG]"
   exit 1
 end
 
@@ -24,38 +27,19 @@ end
 
 # The output file has just been created.
 
-if ARGV[2].to_s == '-o'
-  out_file = File.new(ARGV[3], "w")
-  data = data.unpack("B*")[0]
-  Ais31.procedure_a(data, ARGV[3]) if t == 'A' || t == 'ALL'
-  Ais31.procedure_b(data, ARGV[3]) if t == 'B' || t == 'ALL'
-  Ais31.test_t0(data, ARGV[3])     if t == 'T0'
-  Ais31.test_t1(data, ARGV[3])     if t == 'T1'
-  Ais31.test_t2(data, ARGV[3])     if t == 'T2'
-  Ais31.test_t3(data, ARGV[3])     if t == 'T3'
-  Ais31.test_t4(data, ARGV[3])     if t == 'T4'
-  Ais31.test_t5(data, ARGV[3])     if t == 'T5'
-  Ais31.test_t6(data, ARGV[3])     if t == 'T6'
-  Ais31.test_t7a(data, ARGV[3])    if t == 'T7'
-  Ais31.test_t7b(data, ARGV[3])    if t == 'T7'
-  Ais31.test_t7c(data, ARGV[3])    if t == 'T7'
-  Ais31.test_t8(data, ARGV[3])     if t == 'T8'
-  Ais31.test_t8(data, ARGV[3], 2560000) if t == 'T8L'
-else
-  out_file = File.new("output.txt", "w")
-  data = data.unpack("B*")[0]
-  Ais31.procedure_a(data, "output.txt") if t == 'A' || t == 'ALL'
-  Ais31.procedure_b(data, "output.txt") if t == 'B' || t == 'ALL'
-  Ais31.test_t0(data, "output.txt")     if t == 'T0'
-  Ais31.test_t1(data, "output.txt")     if t == 'T1'
-  Ais31.test_t2(data, "output.txt")     if t == 'T2'
-  Ais31.test_t3(data, "output.txt")     if t == 'T3'
-  Ais31.test_t4(data, "output.txt")     if t == 'T4'
-  Ais31.test_t5(data, "output.txt")     if t == 'T5'
-  Ais31.test_t6(data, "output.txt")     if t == 'T6'
-  Ais31.test_t7a(data, "output.txt")    if t == 'T7'
-  Ais31.test_t7b(data, "output.txt")    if t == 'T7'
-  Ais31.test_t7c(data, "output.txt")    if t == 'T7'
-  Ais31.test_t8(data, "output.txt")     if t == 'T8'
-  Ais31.test_t8(data, "output.txt", 2560000) if t == 'T8L'
-end
+out_file = File.new(outpath, "w")
+data = data.unpack("B*")[0]
+Ais31.procedure_a(data, out_file) if t == 'A' || t == 'ALL'
+Ais31.procedure_b(data, out_file) if t == 'B' || t == 'ALL'
+Ais31.test_t0(data, out_file)     if t == 'T0'
+Ais31.test_t1(data, out_file)     if t == 'T1'
+Ais31.test_t2(data, out_file)     if t == 'T2'
+Ais31.test_t3(data, out_file)     if t == 'T3'
+Ais31.test_t4(data, out_file)     if t == 'T4'
+Ais31.test_t5(data, out_file)     if t == 'T5'
+Ais31.test_t6(data, out_file)     if t == 'T6'
+Ais31.test_t7a(data, out_file)    if t == 'T7'
+Ais31.test_t7b(data, out_file)    if t == 'T7'
+Ais31.test_t7c(data, out_file)    if t == 'T7'
+Ais31.test_t8(data, out_file)     if t == 'T8'
+Ais31.test_t8(data, out_file, 2560000) if t == 'T8L'
